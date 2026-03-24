1bd3c62be9691d18b455358e8adea2821fa81fd5
hastinstagline <hastinstaglne@gmail.com>
2026-03-24 14:43:56 +0530

    add databse connection

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ app = FastAPI(title="Inventory Management API")
 #main app
 app.include_router(product.router)
 app.include_router(supplier.router)
+
+@app.get("/jobs/count")
+def get_job_count():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM jobs")
+    count = cursor.fetchone()[0]
+    cursor.close()
+    conn.close()
+    return {"total_jobs": count}
